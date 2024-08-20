4f8530e25385715e577945974ed2b774d9aafcc3
KHJ <mo481872@gmail.com>
2024-08-20 20:55:19 +0900

    31775

---
+++ BOJ/31775.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        boolean l = false, k = false, p = false;
+        for(int i = 0; i < 3; i++){
+            String str = br.readLine();
+            if(str.startsWith("l")){
+                l = true;
+            } else if (str.startsWith("k")) {
+                k = true;
+            } else if (str.startsWith("p")) {
+                p = true;
+            }
+        }
+        if(l && k && p){
+            System.out.println("GLOBAL");
+        }
+        else {
+            System.out.println("PONIX");
+        }
+    }
+}
