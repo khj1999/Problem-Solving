26a53f9b6230cb704eafdf94de96a7f9a08a3092
KHJ <mo481872@gmail.com>
2024-10-03 14:34:20 +0900

    15654

---
+++ BOJ/15654.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10];
+int result[10];
+bool check[10];
+
+
+void solve(int cnt){
+    if(cnt == m){
+        for(int i = 0; i < m; i++){
+            cout << result[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    else{
+        for(int i = 0; i < n; i++){
+            if(!check[i]){
+                check[i] = true;
+                result[cnt] = arr[i];
+                solve(cnt + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    solve(0);
+    return 0;
+}
