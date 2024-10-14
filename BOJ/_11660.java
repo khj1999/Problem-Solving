30bc5943d32db0d23a03318beb49be813124fd7f
KHJ <mo481872@gmail.com>
2024-10-14 15:54:51 +0900

    11660

---
+++ BOJ/_11660.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class _11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+        int[][] dp = new int[n + 1][n + 1];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j] + arr[i][j];
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int ans = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+            bw.write(ans + "\n");
+        }
+
+        bw.flush();
+    }
+}
