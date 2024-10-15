d7de1dde200127b03df98a7234e5d97ee6165d5f
KHJ <mo481872@gmail.com>
2024-10-15 22:13:57 +0900

    11726

---
+++ BOJ/_11726.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class _11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] % 10007;
+        }
+        long ans = dp[n] % 10007;
+        bw.write(ans + "");
+        bw.flush();
+    }
+}
