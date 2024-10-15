7787c4f5ccf6e486afe94adf12bc052ac22ae78c
KHJ <mo481872@gmail.com>
2024-10-15 22:22:04 +0900

    11726

--- BOJ/_11726.java
+++ BOJ/_11726.java
@@ -5,12 +5,12 @@ public class _11726 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[n + 1];
+        long[] dp = new long[n + 2];
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2] % 10007;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
         long ans = dp[n] % 10007;
         bw.write(ans + "");
